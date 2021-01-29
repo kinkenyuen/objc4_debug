1ab3ca23f17466dadc647765ee6cdb955bbb27f2
ruanjianqin <yuenkinken@icloud.com>
2021-01-29 19:22:32 +0800

    注释

--- objc4-objc4-787.1/objc4/RuntimeApi/main.mm
+++ objc4-objc4-787.1/objc4/RuntimeApi/main.mm
@@ -12,7 +12,7 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
 //        [API aboutClass];
-        [API aboutLibraries];
+//        [API aboutLibraries];
     }
     return 0;
 }
